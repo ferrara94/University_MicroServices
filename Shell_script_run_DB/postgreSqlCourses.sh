bd17262923dbcad6fa99ac9e57557dc0aa7260ac
felice <ing.ferrara94@gmail.com>
2021-11-27 13:38:00 +0100

    course db script added

---
+++ Shell_script_run_DB/postgreSqlCourses.sh
@@ -0,0 +1,9 @@
+cd ../DB_Docker_Configs/PostgreSql_Courses
+
+echo "------------------------------------------------------"
+echo "starting postgresql for Courses Service"
+echo "------------------------------------------------------"
+
+sudo docker-compose up
+
+
